aef0cdd7877bb523f6f256626d85e1538bc447e0
marinakinalone <kinalone.dev@gmail.com>
2021-08-09 17:38:19 +0200

    set up main layout of the app (not finished yet)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="title">
+        <h1>Geeky <span className="pink">Love</span> Letter</h1>
+        <p>because <span className="pink">NERD</span> is the new <span className="pink binary">01010011010001010101100001011001</span></p>
       </header>
+      <div className="main_container">
+        <div className="sub_container" id="text_editor">
+          <h2>text editor</h2>
+          <textarea id="text_content" name="w3review">
+          Write a secret love letter to your nerd crush and send them the binary version.
+          </textarea>
+          <br></br>
+          <label for="text_content">text to be converted in binary language</label>
+        </div>
+        <div className="sub_container" id="binary_generator">
+          <h2>binary generator</h2>
+          <p>01010111011100100110100101110100011001010010000001100001001000000111001101100101011000110111001001100101011101000010000001101100011011110111011001100101001000000110110001100101011101000111010001100101011100100010000001110100011011110010000001111001011011110111010101110010001000000110111001100101011100100110010000100000011000110111001001110101011100110110100000100000011000010110111001100100001000000111001101100101011011100110010000100000011101000110100001100101011011010010000001110100011010000110010100100000011000100110100101101110011000010111001001111001001000000111011001100101011100100111001101101001011011110110111000100001</p>
+        </div>
+      </div>
+      <div className="share_container">
+        <button onclick="#">send your letter</button>
+        <p>or <button onclick="#">copy binary version</button></p>
+      </div>
+      <footer>
+      <p className="copyright">Made with <span className="pink binary">01101100011011110111011001100101</span> by <a href="https://kinalone.dev" target="_blank" rel="noopener noreferrer">MKS</a></p>
+      </footer>
     </div>
   );
 }
