c79ed5a6cd17850129c4adf5a7214ed115cf7322
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-07-29 16:05:46 +0200

    modify  convertToBinary function

--- geeky-love-letter/helpers/convertToBinary.ts
+++ geeky-love-letter/helpers/convertToBinary.ts
@@ -1,19 +1,10 @@
-// https://stackoverflow.com/questions/14430633/how-to-convert-text-to-binary-code-in-javascript
-
 const convertToBinary = (input: string) => {
     let output = [];
     for (let i = 0; i < input.length; i++) {
-        if (input[i] == "\n") {
-            output.push('\n')
-        } else {
-            let bin = input[i].charCodeAt(0).toString(2);
-        output.push(Array(8 - bin.length +1 ).join("0") + bin)
-
-        }
-        
+        const bin = input[i].charCodeAt(0).toString(2);
+        output.push(Array(8 - bin.length + 1 ).join("0") + bin)
     }
     return output.join("")
 }
-
 
 export default convertToBinary
