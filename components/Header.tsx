e4e85211b893fc2cd6b6b69564093e2fae1e13d5
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-08-03 10:07:15 +0200

    modify header animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import cx from 'classnames';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.header__title}>Geeky <span className={styles.pink}>Love</span> Letter</h1>
+      <h1 className={styles.header__title}><span className={styles.title__blue}>Geeky </span><span className={cx(styles.pink, styles.title__pink)}>Love</span><span className={styles.title__blue}> Letter</span></h1>
       <p>because <span className={styles.pink}>NERD</span> is the new <span className={cx(styles.pink, styles.binary)}>01010011010001010101100001011001</span></p>
     </header>
   )
