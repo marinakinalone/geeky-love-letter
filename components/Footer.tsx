27ddb139134bc366da7faddf4e21d10189b94ff4
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-08-03 15:24:44 +0200

    fix typo in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ import { FooterProps } from '../utils/interface';
 const Footer = ({routeMessage, route}: FooterProps) => {
   return (
     <footer className={styles.footer}>
-      <p>Made with <span className={cx(styles.pink, styles.binary)}>0110110001101111011101100110010</span></p>
+      <p>Made with <span className={cx(styles.pink, styles.binary)}>01101100011011110111011001100101</span></p>
        <p>&nbsp;by <a className={styles.link} href="https://kinalone.dev" target="_blank" rel="noopener noreferrer">MKS</a> | <a className={styles.link} href="https://github.com/marinakinalone/geeky-love-letter/blob/main/LICENSE.txt" target="_blank" rel="noopener noreferrer">license</a></p>
        <p>&nbsp;|&nbsp;</p>
        <Link href={route}><a className={styles.link}>{routeMessage}</a></Link>
